34440e753bb02b21d39d4dfe6d4f139ceb634f66
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2023-02-08 15:53:22 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+// Código PHP para obtener el factorial de un número
+// función para obtener factorial de forma iterativa
+function obtieneFactorial($numero){ 
+    $factorial = 1; 
+    for ($i = 1; $i <= $numero; $i++){ 
+      $factorial = $factorial * $i; 
+    } 
+    return $factorial; 
+} 
+   
+//Ejemplo
+$numero = 5; 
+$resultado = obtieneFactorial($numero); 
+echo "Factorial de $numero  = $resultado"; 
+?> 
