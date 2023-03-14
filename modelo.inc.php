cd6e94cd66fae7ad8bbd17dd7fad0069f380b9e7
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2023-03-14 12:03:27 -0500

    Create modelo.inc.php

---
+++ modelo.inc.php
@@ -0,0 +1,15 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
+
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+
+if ($mysqli->connect_error) {
+    die("Fallo la conexion <br> " . $mysqli->connect_error);
+}
+
+
+?>
