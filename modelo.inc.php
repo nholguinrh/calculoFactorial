ed878778081eaea0603e6aa9541a8c065e1a483d
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2023-03-14 12:17:26 -0500

    Update modelo.inc.php

--- modelo.inc.php
+++ modelo.inc.php
@@ -11,5 +11,17 @@ if ($mysqli->connect_error) {
     die("Fallo la conexion <br> " . $mysqli->connect_error);
 }
 
-
+function modeloInsertFactorial($base,$factorial)
+    {
+    global $mysqli;
+    $sql="INSERT INTO factorial (`base`,`factorial`) VALES ($base,$factorial)";
+    $mysqli->query($sql);
+    }
+function modeloListFactorial()
+    {
+    global $mysqli;
+    $sql="SELECT * FROM factorial";
+    $result = $mysqli->query($sql);
+    return($result);
+    }
 ?>
