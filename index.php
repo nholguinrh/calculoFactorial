7d941da7312062f44ae94afef3ecbb030d8f5d8f
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2023-03-14 13:16:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function obtieneFactorial($numero){
    
 
 $numero = (isset($_GET["cantidad"]))? $_GET["cantidad"]:5; 
-
+$suma=0;
 for($i=0;$i<$numero;$i++)
     {
     $n=rand(1,120);
@@ -24,6 +24,7 @@ $salida=modeloListFactorial();
 for($i=0;$i<count($salida,0);$i++)
     {
     $sqrt=ceil(sqrt($salida[$i]["factorial"]));
+    $suma+=ceil(sqrt($salida[$i]["factorial"]));
     echo $i." => El factorial de ".$salida[$i]["base"]." es : ".$salida[$i]["factorial"]." y el entero de la raiz del factorial".$sqrt."\n<br>";
     }
 ?>
