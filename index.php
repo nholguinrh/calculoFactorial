ea71606fac03b52a482c12de0546e86a88a07b58
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2023-03-14 21:10:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-//include "modelo.inc.php";
+include "modelo.inc.php";
 // Código PHP para obtener el factorial de un número
 // función para obtener factorial de forma iterativa
 function obtieneFactorial($numero){ 
@@ -16,19 +16,19 @@ for($i=0;$i<$iteraciones;$i++)
     {
     $n=rand(1,120);
     $resultado = obtieneFactorial($n);
-    /*$sqrt=ceil(sqrt($resultado));
+    $sqrt=ceil(sqrt($resultado));
     $suma+=ceil(sqrt($resultado));
     $k=0;
     for($j=0;$j<$resultado;$j++)
-        $k+=sqrt(obtieneFactorial($n));*/
+        $k+=sqrt(obtieneFactorial($n));
     echo $i." => El factorial de ".$n." es : ".$resultado." y el entero de la raiz del factorial".$sqrt." \r\n <br>";
     }
 echo "suma para consumo de RAM ".$suma." ***************************************************** \n\r <br>";
 //$hostname = gethostname();
-//modeloInsertFactorial($hostname,$suma);
-/*$salida=modeloListFactorial();
+modeloInsertFactorial($hostname,$suma);
+$salida=modeloListFactorial();
 for($i=0;$i<count($salida,0);$i++)
     {
     echo $i." => Desde el pod ".$salida[$i]["hostname"]." | fecha ".date("l",$salida[$i]["hostname"])." | suma:".$suma." \r\n <br>";
-    }*/
+    }
 ?>
